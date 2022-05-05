ac2bfd3f6bb1c5fbe489dcfd74839ed65be110e8
James Pelster <captainswag101@gmail.com>
2022-05-04 22:11:53 -0700

    Add some extra debug information

--- AGC-Sharp/Cpu.cs
+++ AGC-Sharp/Cpu.cs
@@ -251,8 +251,8 @@ namespace AGC_Sharp
             // Print subinstruction debug info
             Console.WriteLine();
             Console.WriteLine($"{subinstructionName}");
-            Console.WriteLine($"Z = {Convert.ToString(RegisterZ, 8)}, A = {Convert.ToString(RegisterA, 8)}, L = {Convert.ToString(RegisterL, 8)}, B = {Convert.ToString(RegisterB, 8)},");
-            Console.WriteLine($"S = {Convert.ToString(RegisterS, 8)}, G = {Convert.ToString(RegisterG, 8)}, Q = {Convert.ToString(RegisterQ, 8)}, SQ = {Convert.ToString(RegisterSQ, 8)},");
+            Console.WriteLine($"Z = {Convert.ToString(RegisterZ, 8)}, A = {Convert.ToString(RegisterA, 8)}, L = {Convert.ToString(RegisterL, 8)}, B = {Convert.ToString(RegisterB, 8)}, EXTEND = {Extend}, INHINT = {InhibitInterrupts}");
+            Console.WriteLine($"S = {Convert.ToString(RegisterS, 8)}, G = {Convert.ToString(RegisterG, 8)}, Q = {Convert.ToString(RegisterQ, 8)}, SQ = {Convert.ToString(RegisterSQ, 8)}, WL = {WriteBus}");
             Console.WriteLine($"EB = {Convert.ToString(RegisterEB >> 8, 8)}, FB = {Convert.ToString(RegisterFB >> 10, 8)}, BB = {Convert.ToString(RegisterBB, 8)}, ST = {Convert.ToString(RegisterST, 8)}, BR1 = {RegisterBR1}, BR2 = {RegisterBR2}");
 
             // Execute subinstruction (queue its control pulses)
